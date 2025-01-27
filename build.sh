90a911a51f5ad595033f8fa10341ce0b234d28d5
MURAKI Taichi <muraki.taichi@gmail.com>
2025-01-27 14:24:32 +0900

    fix

--- build.sh
+++ build.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-# set a build directory name
 BUILD_DIR=build
-emcmake cmake -B $BUILD_DIR -S . -DCMAKE_BUILD_TYPE=Release
-cmake --build $BUILD_DIR
+
+build_em() {
+    emcmake cmake -B $BUILD_DIR -S . -DCMAKE_BUILD_TYPE=Release
+    cmake --build $BUILD_DIR
+}
+
+opencv_build_wasm() {
+    cd opencv
+    mkdir -p build
+    cd build
+
+}
+
+
+# switch by the first argument
+case $1 in
+    em)
+        build_em
+        ;;
+    opencv)
+        opencv_build_wasm
+        ;;
+    *)
+        echo "Usage: $0 {em|opencv}"
+        exit 1
+esac
